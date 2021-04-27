0a0c6cbdb3c4e39da010b88904cff95647d1ed3c
hugo <hugo@KALI.study>
2021-04-27 07:17:35 -0400

    initial commit

---
+++ SudokuSolver.py
@@ -0,0 +1,25 @@
+#Sudoku 2.0
+#TODO
+#Fazer o algoritmo de resolução em si
+#Fazer a GUI
+
+
+
+def main():
+
+	board = init_board()
+	
+	#get board inputs
+
+	#solve board
+
+
+def init_board():
+	board = []
+	for i in range(81):
+		board.append(0)
+	return board
+
+if __name__ == "__main__":
+	main()
+
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Personal project 2
+Improved version of Sudoku-Brute-force-solver
+
+goals:
+Include GUI
+Make code more object oriented
+Include Other solving algorithms
